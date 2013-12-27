20760c6be2ec287238301df26436adb5c79711c0
Kishan Karunaratne <kishan@karu.io>
2013-12-26 21:22:37 -0800

    barebones Tasks index

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,26 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import play.api.data._
+import play.api.data.Forms._
+import models.Task
+
 object Application extends Controller {
 
   def index = Action {
-    Ok("Hello world")
+    Redirect(routes.Application.tasks)
   }
+
+  val taskForm = Form(
+    "label" -> nonEmptyText
+  )
+
+  def tasks = Action {
+    Ok(views.html.index(Task.all(), taskForm))
+  }
+
+  def newTask = TODO
+
+  def deleteTask(id: Long) = TODO
 
 }
