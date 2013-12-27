7b0643f15c3dcaa772fef5354a727d6dd2c1630f
Kishan Karunaratne <kishan@karu.io>
2013-12-26 21:44:43 -0800

    added create() for tasks with persistent H2 DB

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,6 +7,9 @@ import play.api.data._
 import play.api.data.Forms._
 import models.Task
 
+import anorm._
+import anorm.SqlParser._
+
 object Application extends Controller {
 
   def index = Action {
@@ -17,11 +20,21 @@ object Application extends Controller {
     "label" -> nonEmptyText
   )
 
+
+
   def tasks = Action {
     Ok(views.html.index(Task.all(), taskForm))
   }
 
-  def newTask = TODO
+  def newTask = Action { implicit request =>
+    taskForm.bindFromRequest.fold(
+      errors => BadRequest(views.html.index(Task.all(), errors)),
+      label => {
+        Task.create(label)
+        Redirect(routes.Application.tasks)
+      }
+    )
+  }
 
   def deleteTask(id: Long) = TODO
 
